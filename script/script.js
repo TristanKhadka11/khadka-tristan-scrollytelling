e4e4a6180c4c71c415a29165bfc09704808c20fd
TristanKhadka11 <145160199+TristanKhadka11@users.noreply.github.com>
2024-04-10 19:19:55 -0400

    2.2a v3

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
-// Import de la bibliothèque GSAP et ScrollTrigger (assurez-vous qu'ils sont correctement liés dans votre HTML)
+
 gsap.registerPlugin(ScrollTrigger);
 
-// Animation de l'icône en boucle
+
 gsap.to('.scroll-down img', {
     duration: 1,
     y: 10,
