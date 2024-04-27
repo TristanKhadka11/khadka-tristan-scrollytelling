693c2f2af122d5404f24b0b1eb8bc5806907eee0
TristanKhadka11 <145160199+TristanKhadka11@users.noreply.github.com>
2024-04-27 19:13:52 -0400

    2.2b v2

--- script/script.js
+++ script/script.js
@@ -30,6 +30,107 @@ window.addEventListener('scroll', function() {
     }, 100);
 });
 
+const timelineTitre = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#chapitre1", 
+      start: "top center",   
+      end: "center center", 
+      scrub: true            
+    }
+  });
+  
+
+  timelineTitre.from("#titre", {
+    opacity: 0,          
+    x: -100,           
+    duration: 1,        
+    ease: "power2.inOut" 
+  });
+
+
+  timelineTitre.to("#soleil", {
+    x: -window.innerWidth, 
+    duration: 3,           
+    ease: "power2.inOut"  
+  });
+
+  timelineTitre.from("#lune", {
+    opacity: 0,
+    x: window.innerWidth,
+    duration: 3,
+    ease: "power2.inOut"
+  });
+
+
+
+const timeline2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#chapitre2",
+      start: "top center",   
+      end: "center center", 
+      scrub: true            
+    }
+  });
+  
+
+  timeline2.from("#z1", { opacity: 0, duration: 0.5 })
+          .from("#z2", { opacity: 0, duration: 0.5 })
+          .from("#z3", { opacity: 0, duration: 0.5 });
+
+         
+timeline2.from(".story-text2", { opacity: 0, duration: 1 });
+
+timeline2.fromTo(".sprite1", { 
+  scale: 0, 
+  
+}, { 
+  scale: 0.8, 
+
+  duration: 0.5 
+});
+
+const timeline3 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#chapitre4", 
+      start: "top center",  
+      end: "center center", 
+      scrub: true           
+    }
+  });
+  
+ 
+  timeline3.fromTo("#champignon", {
+    skewX: 0, 
+    skewY: 0  
+  }, {
+    skewX: 9,
+    skewY: 15, 
+    duration: 1 
+  });
+
+  const timeline4 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#chapitre5", 
+      start: "top center",   
+      end: "center center",  
+      scrub: true            
+    }
+  });
+  
+
+  
+
+  timeline4.to("#porte", {
+    x: tremblementX, 
+    y: tremblementY,
+    duration: 0.1,   
+    repeat: -1,      
+    yoyo: true       
+  });
+
+  
+
+
 
 
 
